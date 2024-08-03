6c66613122c2473d65c685524ba544fe38edf0c4
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-08-03 21:55:23 +0400

    Update ft_printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,10 +1,16 @@
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int ft_printf(const char *format, ...);
+int	ft_put_hex(unsigned long long n, int uppercase);
+int	ft_put_ptr(void *ptr);
+int	ft_put_unsigned(unsigned int n);
+int	ft_putchar(char c);
+int	ft_putnbr(int n);
+int	ft_putstr(const char *str);
+int	ft_printf(const char *format, ...);
 
 #endif
