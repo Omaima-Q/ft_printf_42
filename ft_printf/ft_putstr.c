c5ae87815339b0f952ccc465a1f32fcf62dde125
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-08-03 21:50:13 +0400

    Create ft_putstr.c

---
+++ ft_printf/ft_putstr.c
@@ -0,0 +1,16 @@
+#include "ft_printf.h"
+
+int	ft_putstr(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (str[i])
+		i += ft_putchar(str[i]);
+	return (i);
+}
